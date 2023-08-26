cf4931971a58537053436f8da6bc8630815cace6
Hashan Danushka <hashansooriyage@gmail.com>
2023-08-26 22:51:28 +0530

    Added slash on routes

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::post('auth/register', [AuthController::class, 'register']);
+Route::post('/auth/register', [AuthController::class, 'register']);
 
-Route::post('auth/login', [AuthController::class, 'login']);
+Route::post('/auth/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
